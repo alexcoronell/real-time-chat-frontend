204ca7aa6d89f66d7a7990142081ca3a266b81b1
Alex Coronell <alexcoronell@gmail.com>
2025-08-13 09:44:26 -0500

    wouter installed, basic pages created

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import './App.css'
+import { Route, Switch } from 'wouter';
+
+/* Pages */
+import ChatPage from './pages/ChatPage';
+import LoginPage from './pages/LoginPage';
+
+import './App.css';
 
 function App() {
-  
   return (
     <>
       <h1>Real Time Chat</h1>
+      <Switch>
+        <Route path='/' component={LoginPage} />
+        <Route path='/chat' component={ChatPage} />
+      </Switch>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
